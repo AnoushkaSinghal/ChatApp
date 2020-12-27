214e43af48323fae474b9f2c37da5a298671c164
AnoushkaSinghal <61997251+AnoushkaSinghal@users.noreply.github.com>
2020-12-26 21:28:12 -0500

    added a bot
    Added a bot that checks the chat for weather?, time? or greeting and replies back

--- server.js
+++ server.js
@@ -1,29 +1,87 @@
-var express = require('express');
-var app = express();
+let express = require("express");
+let app = express();
 
-var http = require('http');
-var server = http.Server(app);
+let http = require("http");
+const { networkInterfaces } = require("os");
+let server = http.Server(app);
 
-app.use(express.static('client'));
+app.use(express.static("client"));
+let io = require("socket.io")(server);
 
-var io = require('socket.io')(server);
+let messageArhive = [];
 
-io.on('connection', function (socket) {
-  socket.on('message', function (msg) {
-    io.emit('message', msg);
+const isQuestion = (msg) => {
+  return msg.match(/\?$/);
+};
+
+const askingTime = (msg) => {
+  return msg.match(/time/i);
+};
+
+const askingWeather = (msg) => {
+  return msg.match(/weather/i);
+};
+
+const isGreating = (msg) => {
+  if (msg.match(/hi/i) || msg.match(/hello/i)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const getWeather = (callback) => {
+  let request = require("request");
+  request.get("https://www.metaweather.com/api/location/4118/",
+    function (error, response) {
+      if (!error && response.statusCode == 200) {
+        let data = JSON.parse(response.body);
+        callback(data.consolidated_weather[0].weather_state_name);
+      }
+    }
+  );
+};
+let botAnswer = "TimeBot: ";
+
+io.on("connection", function (socket) {
+  messageArhive.map((message) => {
+    socket.send(message);
   });
-    socket.on('initial', function (msg) {
-    io.emit('intial', msg);
+
+  socket.on("message", function (msg) {
+    messageArhive.push(msg);
+
+    if (!isQuestion(msg)) {
+      if (isGreating(msg)) {
+        io.emit("message", msg);
+        setTimeout(() => {
+          let userName = msg.split(" ");
+          io.emit(
+            "message", botAnswer + "Hi " + userName[0] + "! Ask me the weather or time."
+          );
+        }, 800);
+      } else {
+        io.emit("message", msg);
+      }
+    } else if (askingTime(msg)) {
+      io.emit("message", msg);
+      setTimeout(() => {
+        io.emit("message", botAnswer + "It is "+ new Date().toLocaleTimeString() + " in your timezone.");
+      }, 800);
+    } else if (askingWeather(msg)) {
+      io.emit("message", msg);
+      getWeather(function (weather) {
+        io.emit("message", botAnswer + " It is going to be " + weather.toLowerCase() + " right now.");
+      }, 800);
+    }
   });
-    
 });
-
 
 let port = process.env.PORT;
 if (port == null || port == "") {
-    port = 8080;
+  port = 8080;
 }
 
 server.listen(port, function () {
-    console.log("Chat server running on port: http://localhost:8080");
+  console.log("Chat server running on port: http://localhost:8080");
 });
